1b9a8b64da23cb1f13a309bac6abb57f6c54ebde
YaronDavid <urlof_desty91@hotmail.com>
2023-04-11 10:42:58 -0600

    adding pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { counterModule } from './counter/counter.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 
 @NgModule({
@@ -12,7 +13,13 @@ import { counterModule } from './counter/counter.module';
   ],
   imports: [
     BrowserModule,
-    counterModule
+    counterModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
