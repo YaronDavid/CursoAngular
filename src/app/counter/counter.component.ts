324302bcc574cba47e06ee6b888c837ea73d8c50
YaronDavid <urlof_desty91@hotmail.com>
2023-04-11 09:56:03 -0600

    adding styles

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -2,7 +2,8 @@ import {Component} from "@angular/core"
 
 @Component({
     selector: "app-contador",
-    templateUrl: "./counter.component.html"
+    templateUrl: "./counter.component.html",
+    styleUrls: ["./counter.component.css"]
 })
 export class counterComponent{
     contador:number = 23;
